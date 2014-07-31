5ae376ca8cb53308b21e0f0e85454405410cd9de
tanatana <tugusuke@gmail.com>
2014-07-31 21:06:08 +0900

    WIP: experimental implement \w escape sequence

--- screen.go
+++ screen.go
@@ -24,14 +24,19 @@ func (s *screen)initialize() {
 }
 
 func (s *screen)Flush() {
-
+	fmt.Printf("\033[2J");
+	fmt.Printf("\033[0;0H");
 }
 
 func (s *screen)Start() {
 	go func() {
+		num := 0
 		for {
-			s.Print()
-			time.Sleep(100 * time.Millisecond)
+			s.Flush()
+			// s.Print()
+			s.Print2(num)
+			time.Sleep(time.Duration(1000/s.fps) * time.Millisecond)
+			num += 1
 		}
 	}()
 }
@@ -42,5 +47,9 @@ func (s *screen)Stop(){
 
 func (s *screen)Print() {
 	fmt.Println("Hello")
+}
 
+func (s *screen)Print2(num int) {
+	fmt.Printf("\033[%d;%dH", num % s.height, (num * 2) % s.width);
+	fmt.Println("Hello")
 }
