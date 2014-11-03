594817cf4f5b2cdb0d7e23a02925d9292665b1cb
tanatana <tugusuke@gmail.com>
2014-11-04 03:01:28 +0900

    kickout magick number

--- split_pane.go
+++ split_pane.go
@@ -1,63 +1,63 @@
 package aspirin
 
-import (
-	"fmt"
-)
-
 type SplitPane struct {
 	BasePane
 	Type SplitType
-	Text string
 }
 
 func NewSplitPane(splitPaneId int,targetPane Pane, splitType SplitType) (Pane, PaneSize, PaneSize){
 	var sp Pane
 	var leftPaneSize, rightPaneSize PaneSize
 
+	sp = new(SplitPane)
+	sp.setId(splitPaneId)
+
 	if (splitType == SplitVirtical) {
-		sp = new(SplitPane)
-		// sp.setId(win.paneCounter)
-		sp.setId(splitPaneId)
-		sp.setSize(targetPane.Size().x + targetPane.Size().width/2,
-			targetPane.Size().y,
-			1,
-			targetPane.Size().height)
+		spX := targetPane.Size().x + targetPane.Size().width/2
+		spY := targetPane.Size().y
+		spWidth := 1
+		spHeight := targetPane.Size().height
+
+		sp.setSize(spX, spY, spWidth, spHeight)
 
 		leftPaneSize.x       = targetPane.Size().x
 		leftPaneSize.y       = targetPane.Size().y
 		leftPaneSize.width   = targetPane.Size().width/2
 		leftPaneSize.height  = targetPane.Size().height
 
-		rightPaneSize.x      = targetPane.Size().x + targetPane.Size().width/2 + 1
-		rightPaneSize.y      = targetPane.Size().y
-		rightPaneSize.width  = targetPane.Size().width/2 - 1
+		rightPaneSize.x      = spX + spWidth
+		rightPaneSize.y      = spY
+		rightPaneSize.width  = targetPane.Size().width/2 - spWidth
 		rightPaneSize.height = targetPane.Size().height
 
 		if (targetPane.Size().width % 2 == 1) {
-			rightPaneSize.width += 1
+			rightPaneSize.width += spWidth
 		}
 	} else if (splitType == SplitHorizontal) {
-		sp = new(SplitPane)
-		sp.setId(splitPaneId)
-		sp.setSize(targetPane.Size().x, targetPane.Size().y + targetPane.Size().height/2, targetPane.Size().width, 1)
+		spX := targetPane.Size().x
+		spY := targetPane.Size().y + targetPane.Size().height/2
+		spWidth := targetPane.Size().width
+		spHeight := 1
+
+		sp.setSize(spX, spY, spWidth, spHeight)
 
 		leftPaneSize.x       = targetPane.Size().x
 		leftPaneSize.y       = targetPane.Size().y
 		leftPaneSize.width   = targetPane.Size().width
 		leftPaneSize.height  = targetPane.Size().height/2
 
-		rightPaneSize.x      = targetPane.Size().x
-		rightPaneSize.y      = targetPane.Size().y + targetPane.Size().height/2 + 1
+		rightPaneSize.x      = spX
+		rightPaneSize.y      = spY + spHeight
 		rightPaneSize.width  = targetPane.Size().width
-		rightPaneSize.height = targetPane.Size().height/2 - 1
+		rightPaneSize.height = targetPane.Size().height/2 - spHeight
 
 		if (targetPane.Size().height % 2 == 1) {
-			rightPaneSize.height += 1
+			rightPaneSize.height += spHeight
 		}
 	}
+
 	return sp, leftPaneSize, rightPaneSize
 }
 
 func (rp *SplitPane)viewDidLoad() {
-	fmt.Printf("viewDidLoad@%s\n", "SplitPane")
 }
